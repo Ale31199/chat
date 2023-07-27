3ee827228bd775d6eda3a5dad8c3da82acd96283
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-07-27 17:49:54 +0200

    new prcjt

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./style.css";
+
+export default function App() {
+  const [textlines, settextlines] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const loop = () => {
+    setTimeout(() => {
+      const inputElement = document.getElementById("msg") as HTMLInputElement;
+      if (inputElement) {
+        let textpush = inputElement.value;
+        const newline = textpush.trim();
+        settextlines([...textlines, newline]);
+        setInputValue("");
+      }
+    }, 450);
+  };
+
+  const removeline = () => {
+    setTimeout(() => {
+      const remove = [...textlines];
+      remove.pop();
+      settextlines(remove);
+    }, 450);
+  };
+
+  const enter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      loop();
+    }
+  };
+
+  useEffect(() => {
+    const scroll = scrollRef.current;
+    if (scroll) {
+      scroll.scrollTop = scroll.scrollHeight;
+    }
+  }, [textlines]);
+
+  return (
+    <div className="App">
+      <h1>Chat Test Alpha</h1>
+      <h2>Developing something interesting!</h2>
+      <div className="chatbox">
+        <input
+          className="bar"
+          id="msg"
+          type="text"
+          placeholder="Write something..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyPress={enter}
+        />
+      </div>
+      <button className="send" id="send" onClick={loop}>
+        Send
+      </button>
+      <div className="textbox" ref={scrollRef}>
+        {textlines.map((line, index) => (
+          <p className="messages" key={index} id="messages">
+            {line}
+          </p>
+        ))}
+      </div>
+
+      <button className="delete" id="delete" onClick={removeline}>
+        DELETE LAST MESSAGE
+      </button>
+    </div>
+  );
+}
