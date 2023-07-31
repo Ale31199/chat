3d5cae8f1c4572f2edb5d0ba6f40f172cb3b9110
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-07-31 14:06:08 +0200

    aggiunto ROUTER, FIREBASE E REGISTER

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import registerPage from './registerPage';
+
+
+
+
+
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
