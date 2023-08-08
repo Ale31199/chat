e05ccc3df7659c127c59fce92ef07995203b5adf
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-08-08 21:04:17 +0200

    file php eliminati + collegamento a MYSQL riusicto!!

---
+++ INSERT INTO myguests (id, firstname, las.sql
@@ -0,0 +1,8 @@
+INSERT INTO myguests (id, firstname, lastname, email, reg_date)
+VALUES (
+    id:int,
+    'firstname:varchar',
+    'lastname:varchar',
+    'email:varchar',
+    'reg_date:timestamp'
+  );
